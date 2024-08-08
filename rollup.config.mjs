678b41848a38307bef78c1015c90a25e825c869c
梁志伟 <314607329@qq.com>
2024-08-08 16:24:12 +0800

    🎉 发布 v0.0.1 预览版本

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import cleaner from "rollup-plugin-cleaner";
+import {terser} from "rollup-plugin-terser";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/index.js",
+            format: "iife",
+            name: "store"
+        },
+        {
+            file: "dist/index.esm.js",
+            format: "esm"
+        }
+    ],
+    plugins: [
+        cleaner({
+            targets: ["./dist/"]
+        }),
+        resolve(),
+        commonjs(),
+        typescript({
+            useTsconfigDeclarationDir: true
+        }),
+        terser() // 压缩代码
+    ]
+};
